b0610af9c31bd35d756748bf807e63292bb4b52c
4everBEKO <ulugbekorozboyev2@gmail.com>
2024-01-26 15:27:39 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+"use strict";
+// let number = 100;
+// const num = 100;
+
+// const isNotMassiv = {
+//     age: 10,
+//     clientName: "John",
+//     surName: "Jonathan",
+//     address: "Johnston street",
+//     phoneNumber: 929192911
+// };
+
+// console.log(isNotMassiv);
+
+
+// const isMassiv = [10, "John", "Jonathan", "Johnston street", 929192911];
+
+// console.log(isMassiv);
+
+
+// alert("Are you ?");
+
+// const place = confirm("Are you muslim?");
+// console.log(place);
+
+// const currentPlace = +prompt("Where are you?", "");
+// console.log(typeof currentPlace + 10);
+
+
+const channel = prompt("what's your favourite channel?");
+console.log(`https://youtube.com/${channel}`);
+
+const user = "Johnston";
+const age = 10;
+
+console.log(`My name is ${user}, age is ${age}`);
